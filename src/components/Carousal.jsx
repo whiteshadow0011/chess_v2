0dfe1956f1d08d8969030b761ce9ef33cfb5f448
Om Zade <151941644+whiteshadow0011@users.noreply.github.com>
2025-01-06 11:53:15 +0530

    changes in carousal

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import Image1 from "src/assets/pexels-cottonbro-4973821.jpg";
+import Image2 from "src/assets/pexels-recalmedia-42070.jpg";
+import Image3 from "src/assets/pexels-vlada-karpovich-6114992.jpg";
 
 const ThreeBoxesPage = () => {
   const boxes = [
     {
-      image: "src/assets/pexels-cottonbro-4973821.jpg",
+      image: Image1,
       text: "This is the first achievement",
     },
     {
-      image: "src/assets/pexels-recalmedia-42070.jpg",
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, maxime ex dignissimos vel alias facilis numquam nesciunt suscipit animi accusamus.',
+      image: Image2,
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, maxime ex dignissimos vel alias facilis numquam nesciunt suscipit animi accusamus.",
     },
     {
-      image: "src/assets/pexels-vlada-karpovich-6114992.jpg",
-      text:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, totam. Sit sapiente magni nisi quaerat autem nihil dolorem animi rem, laborum tenetur dolores, quisquam labore accusantium repellendus rerum voluptas illum blanditiis aliquam facere! Molestias!',
+      image: Image3,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, totam. Sit sapiente magni nisi quaerat autem nihil dolorem animi rem, laborum tenetur dolores, quisquam labore accusantium repellendus rerum voluptas illum blanditiis aliquam facere! Molestias!",
     },
   ];
 
